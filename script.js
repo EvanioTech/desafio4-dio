e77d081fe322c4b0de8eb355b351dc6c65f19aff
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2023-12-06 10:31:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let num1 = parseInt(gets(1));
+let num2 = parseInt(gets(2));
+
+let calc = num1 * num2 * 100
+
+//TODO: Implemente a lógica para exibir o xpGanho:
+const xpGanho = calc;
+
+
+
+
+
+function calcLvBatle(porradaria) {
+
+ 
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Imprime a quantidade de XP ganho
+console.log("Voce ganhou " + xpGanho + " XP!");
